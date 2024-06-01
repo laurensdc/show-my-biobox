5bf2b6f42271b9a22823a8b887f46a6c84c59c90
Laurens DC <laurensdc@gmail.com>
2024-06-01 13:13:59 +0200

    feat: only show bioboxes of last 3 weeks

--- files.js
+++ files.js
@@ -48,14 +48,34 @@ export function writeFile(article) {
 }
 
 export const getNavElement = () => {
-  const filesInBioboxes = fs.readdirSync(bioboxDir);
+  const filesInBioboxesDir = fs.readdirSync(bioboxDir);
 
-  let nav = '';
-  filesInBioboxes.forEach(file => {
+  // Only use files of last 21 days, files are in the format of YYYY-MM-dd.html
+  const filesOfLast7Days = filesInBioboxesDir.filter(file => {
+    const date = new Date(file.replace(/\.html$/, ''));
+    console.dir(date, { depth: null, colors: true });
+    const now = new Date();
+    const diff = Math.abs(now - date);
+    const diffDays = Math.ceil(diff / (1000 * 60 * 60 * 24));
+    return diffDays <= 21;
+  });
+
+  // Sort chronologically
+  filesOfLast7Days.sort((a, b) => {
+    const dateA = new Date(a.replace(/\.html$/, ''));
+    const dateB = new Date(b.replace(/\.html$/, ''));
+    return dateB - dateA;
+  });
+
+  console.dir({ filesOfLast7Days }, { depth: null, colors: true });
+
+  let nav = '<ul>';
+  filesOfLast7Days.forEach(file => {
     const removeDotHtml = file.replace(/\.html$/, '');
-
-    nav += `<a href="/${file}">${removeDotHtml}</a> `;
+    nav += `<li><a href="/${file}">${removeDotHtml}</a></li> `
   })
+
+  nav += '</ul>';
 
   return nav;
 }

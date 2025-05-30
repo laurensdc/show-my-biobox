7afaf375443abf8c3589c421285f6d2868dfb7ce
Laurens DC <coding@laurensdc.com>
2025-05-30 17:14:03 +0200

    🧪 prepare test cases

---
+++ biobox.test.js
@@ -0,0 +1,12 @@
+import { it, describe } from "node:test";
+import { equal } from "node:assert"
+
+describe("fetchRelevantArticleAsHTML", () => {
+  it.todo("extracts the snippet where <h1> contains 'groente & fruit'")
+
+  it.todo("declutters links from the article")
+
+  it.todo("adds links to the recipes")
+
+  it.todo("highlights the current week")
+})
